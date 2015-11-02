527fba5f02d1f4652c31625b9a77ddfb81d064eb
ChinLan <nilsoul@hotmail.com.tw>
2015-11-02 14:08:51 +0800

    syntax revise

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -18,7 +18,7 @@ class Shot < ActiveRecord::Base
                              url: '/system/:class/:attachment/:id_partition/:style/:hash.:extension',
                              path: ':rails_root/public/system/:class/:attachment/:id_partition/:style/:hash.:extension',
                              hash_secret: 'get_from_rake_secret'
-                    :attachment, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"
+                    :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :s3_host_name => "s3-ap-northeast-1.amazonaws.com"
   validates_attachment :photo, content_type: { content_type: /\Aimage\/.*\Z/ },
                                 size: { in: 0..1.megabytes }
 
