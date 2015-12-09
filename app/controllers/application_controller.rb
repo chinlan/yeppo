d7cdfe556679400520dc08f36cd3411eb14f27c4
ChinLan <nilsoul@hotmail.com.tw>
2015-12-09 19:28:41 +0800

    back_to_same_page_after_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,14 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:account_update) << :location
       devise_parameter_sanitizer.for(:account_update) << :content
 
+  end
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
     end
+  end
 end
