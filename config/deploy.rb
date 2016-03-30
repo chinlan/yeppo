55e96c67b7d95a3bbafb0106b6880ce7c3f1c26a
ChinLan <nilsoul@hotmail.com.tw>
2016-03-30 17:52:17 +0800

    revise back

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,8 +28,8 @@ set :passenger_restart_with_touch, true
 # set :pty, true
 
 # Default value for :linked_files is []
- #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/facebook.yml', 'config/email.yml', 'config/s3.yml')
- set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/facebook.yml', 'config/s3.yml')
+ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/facebook.yml', 'config/email.yml', 'config/s3.yml')
+ 
 # Default value for linked_dirs is []
  set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
